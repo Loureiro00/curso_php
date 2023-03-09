627fe3d064683086db45048be09bbb3adcb58173
Loureiro00 <pedrohstloureiro@gmail.com>
2023-03-09 15:28:08 -0300

    Aula 06

---
+++ 6_ESTRUTURAS_DE_CONTROLE/1_if/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// condição verdadeira
+if ( 5 > 2) { 
+
+    echo " Deu certo, entrou no if <br>";
+}
+
+// condição verdadeira
+if ( 5 < 2) { 
+
+    echo " Não deu certo, nao esta no laço. <br>";
+}
+
+// utilizar op. lógico
+
+if (10 === 10 && 9 > 3 ){
+   
+   echo " Deu certo, entrou no if 2  <br>";
+  
+
+
+}
