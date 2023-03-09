efb439ac98a7ff7edf700d36bc373e7a64b13f4b
Loureiro00 <pedrohstloureiro@gmail.com>
2023-03-09 16:53:34 -0300

    06

--- 6_ESTRUTURAS_DE_CONTROLE/1_if/index.php
+++ 6_ESTRUTURAS_DE_CONTROLE/1_if/index.php
@@ -21,3 +21,15 @@ if (10 === 10 && 9 > 3 ){
 
 
 }
+
+
+// variaveis 
+$a = 10 ;
+$b = 5 ;
+
+$c = "Deu certo, entrou no if 3 <br>";
+
+
+if ($a >= $b ){
+    echo $c ;
+}
---
+++ 6_ESTRUTURAS_DE_CONTROLE/2_exercicio_21/index.php
@@ -0,0 +1,34 @@
+<?php
+/*
+
+    Faça as seguintes verificações em if :
+    5 é maior que 2 ?
+    Matheus é diferente de Pedro
+    12 é menor ou igual a 11
+    voce deve inserir os valores em variaveis;
+
+
+*/
+
+
+$a = 5;
+$b = 2;
+
+$c = "Matheus";
+$d = " Pedro";
+
+
+if ( $a > $b ){
+    echo " A variável A é maior que B <br>";
+
+}
+
+if ( $c != $d ){
+    echo " O valor é diferente   <br> ";
+
+}
+
+
+
+
+
